f24c16706c2883c64696397be896e606e6c40571
Arbaj Shaikh <sarbaj637@gmail.com>
2025-10-09 14:51:45 +0530

    created user and video model with jwt token

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
 dotenv.config({
   path: "./.env",
 });
-import mongoose from "mongoose";
+
 import connectDB from "./db/index.js";
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.listen(port, async () => {
-  await connectDB();
-  console.log(`DB connected and running on http://localhost:${port}`);
-});
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`DB connected and running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!! " + err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello jiii");
